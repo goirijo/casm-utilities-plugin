592c5a7fde2382d281eff896e2c75fda9c4c7b6a
John Goiri <jg.goiri@gmail.com>
2020-10-15 20:13:45 -0700

    Added a fake test

---
+++ hello.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    std::cout<<"Get in loser, we're going shopping.\n";
+    return 0;
+}
---
+++ test/unit/hello.cpp
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+
+TEST_F(ExampleTest, AlwaysPasses)
+{
+    EXPECT_TRUE(true);
+}
